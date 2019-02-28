741710b3407d5bb8d9ec030ad3d10197e0086e59
Mark Packham <markpackham1@gmail.com>
2019-02-28 19:25:22 +0000

    one line if statement

--- rubyExamples.rb
+++ rubyExamples.rb
@@ -22,7 +22,9 @@ puts name.downcase.reverse.upcase
 puts "timmy".capitalize!
 
 
-#Get data, if, elsif, else
+#Get data, if, elsif, else & case (Ruby's answer to a switch statement)
+puts "It's true!" if true #one line if statement
+
 print "Integer please: "
 user_num = Integer(gets.chomp)
 if user_num < 0
@@ -31,6 +33,17 @@ elsif user_num > 0
   puts "You picked a positive integer!"
 else
   puts "You picked zero!"
+end
+
+case language
+when "JS"
+  puts "Websites!"
+when "Python"
+  puts "Science!"
+when "Ruby"
+  puts "Web apps!"
+else
+  puts "I don't know!"
 end
 
 
@@ -211,3 +224,86 @@ movie_ratings = {
 }
 movie_ratings.each_key { |k| puts "#{k}"}
 movie_ratings.each_value { |v| puts "#{v}"}
+
+
+
+movies = {
+  StarWars: 4.8, 
+  Divergent: 4.7
+  }
+
+puts "What would you like to do? "
+
+choice = gets.chomp
+
+case choice
+when "add"
+  puts "What movie would you like to add? "
+  title = gets.chomp
+  puts "What rating does the movie have? "
+  rating = gets.chomp
+  movies[title.to_sym] = rating.to_i
+when "update"
+  puts "Updated!"
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
+end
+
+
+#CRUD
+movies = {
+  Memento: 3,
+  Primer: 4,
+  Ishtar: 1
+}
+puts "What would you like to do?"
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+choice = gets.chomp.downcase
+case choice
+when 'add'
+  puts "What movie do you want to add?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "What's the rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been added with a rating of #{rating}."
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
+when 'update'
+  puts "What movie do you want to update?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Movie not found!"
+  else
+    puts "What's the new rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been updated with new rating of #{rating}."
+  end
+when 'display'
+  movies.each do |movie, rating|
+    puts "#{movie}: #{rating}"
+  end
+when 'delete'
+  puts "What movie do you want to delete?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Movie not found!"
+  else
+    movies.delete(title.to_sym)
+    puts "#{title} has been removed."
+  end
+else
+  puts "Sorry, I didn't understand you."
+end
+
+
