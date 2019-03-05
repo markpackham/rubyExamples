3db8c4584a1adb5cb1addeba932cfd158a9bfbaf
Mark Packham <markpackham1@gmail.com>
2019-03-05 20:10:12 +0000

    Override superclass

--- rubyExamples.rb
+++ rubyExamples.rb
@@ -636,3 +636,20 @@ class SuperBadError < ApplicationError
 end
 err = SuperBadError.new
 err.display_error
+
+
+#Override superclass
+class Creature
+  def initialize(name)
+    @name = name
+  end  
+  def fight
+    return "Punch to the chops!"
+  end
+end
+
+class Dragon <  Creature
+  def fight
+    return "Breathes fire!"
+  end
+end
